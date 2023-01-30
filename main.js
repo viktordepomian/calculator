9231896a29607baa20f10e2c187592e6625f7d11
viktordepomian <viktor.depomian@gmail.com>
2023-01-30 18:23:58 +0100

    Added all the files

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-const resultsWindow = document.querySelector('.results-window');
-const inputBtn = document.querySelectorAll('button');
+const output = document.querySelector(".results-window");
+const btns = document.querySelectorAll(".btn");
+let calculation = "";
 
-inputBtn.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-        e.preventDefault();
-        console.log(e);
-    })
+btns.forEach(btn => {
+  btn.addEventListener("click", e => {
+    const value = e.target.innerText;
+    if (!isNaN(value)) {
+      calculation += value;
+    } else if (value === "C") {
+      calculation = "";
+    } else if (value === "=") {
+      calculation = eval(calculation.replace(/x/g, '*').replace(/÷/g, '/')).toString();
+    } else {
+      calculation += ` ${value} `;
+    }
+      output.innerText = calculation;
+  });
 });
